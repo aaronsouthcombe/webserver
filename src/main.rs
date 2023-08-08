e9d5b1afcd8fffda4a66a54184679f4bb8d6d7a0
Aaron Southcombe <aaronkieransouthcombe@gmail.com>
2023-08-08 21:54:15 +0200

    First edition webserver functional prints incoming req and outgoing res

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    const SERVADDR: &str = "127.0.0.1:7878";
+    let listener: TcpListener = TcpListener::bind(&SERVADDR).unwrap();
+    println!("The server has been started on {}", &SERVADDR);
+
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+        handle_connection(stream);
+        println!("Connection established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response: &str = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+    println!("Response sent: {}", response);
+    println!("Request: {:#?}", http_request)
 }
